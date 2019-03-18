459ad41e5e2ba0a05f2494299fbe6579a4b9aa03
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-18 16:58:42 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+//$('parent-tag child-tag:first-child')
+//$("img[alt='the beatles making faces']")
+//$('div:last')
+/*Write a function ninjaBabySelector that does not accept any parameters.
+ The function should use an ID selector to return the ninja baby image.
+
+ */
+function paragraphSelector() {
+  return $('p');
+}
+function lastImageSelector() {
+  return $('img:last');
+}
+function ninjaBabySelector() {
+  return $('img#baby-ninja');
+}
