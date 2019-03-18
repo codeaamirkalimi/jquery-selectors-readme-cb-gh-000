446a063e899a6408b1b30d1521ecd4f03c0134ed
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-18 17:02:11 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,9 +2,12 @@
 //$('parent-tag child-tag:first-child')
 //$("img[alt='the beatles making faces']")
 //$('div:last')
-/*Write a function divSelector that does not accept any parameters.
- The function should use a class selector to return the two divs with the class pics.
+/*Write a function firstListItem that does not accept any parameters. 
+The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
  */
+ function firstListItem() {
+   return $('ul#pic-list li:first-child');
+ }
 function paragraphSelector() {
   return $('p');
 }
